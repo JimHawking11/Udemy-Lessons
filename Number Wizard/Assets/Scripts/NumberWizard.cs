2fb6bdad845eafb18ed04171479d5539401ee417
Mike Salkin <jimhawking11@gmail.com>
2016-01-17 02:12:39 -0600

    Import Project

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	const int ONE_SECOND = 60;
+	const int TIME_TO_WAIT = 4;
+	
+	private int max;
+	private int min;
+	private int guess;
+	private int framesWaited;
+	private int timeWaited;
+	
+	private bool isInStartUp = false;
+	
+	// Use this for initialization
+	void Start () {
+		//Set Framerate
+		Application.targetFrameRate = ONE_SECOND;
+		
+		//Begin Game
+		StartGame ();
+	}
+
+	void StartGame ()
+	{
+		isInStartUp = true;
+		framesWaited = 0;
+		timeWaited = 0;
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
+		//Start Up Text
+		print ("========================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number from " + min + " to " + max + " in your head, but don't tell me!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (isInStartUp)
+		{
+			//Advance Time
+			framesWaited++;
+			
+			//Advance One Second
+			if(framesWaited >= ONE_SECOND)
+			{
+				print (".");
+				timeWaited++;
+				framesWaited = 0;
+			}
+			
+			//End Startup
+			if(timeWaited >= TIME_TO_WAIT)
+			{
+				isInStartUp = false;
+				print ("OK got it?");
+				NextGuess();
+			}
+		}else{
+			if (Input.GetKeyDown (KeyCode.UpArrow))
+			{
+				min = guess + 1;
+				//Validate for edge case
+				if(min > max)
+				{
+					min = max;
+				}
+				NextGuess();
+			}
+			else if (Input.GetKeyDown (KeyCode.DownArrow))
+			{
+				max = guess - 1;
+				//Validate for edge case
+				if(max < min)
+				{
+					max = min;
+				}
+				NextGuess();
+			}
+			else if (Input.GetKeyDown (KeyCode.Return))
+			{
+				print ("I Won!");
+				StartGame ();
+			} 
+		} 
+	}
+	
+	void NextGuess()
+	{
+		print ("**Range is " + min + " to " + max);
+		
+		guess = Random.Range(min, max + 1);
+		print ("Is the number higher or lower then " + guess + "?");
+		print ("UP = higher, DOWN = lower, RETURN = equals");
+	}
+}
---
+++ Text101/Assets/TextController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Reflection;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	
+	private enum States {cell, panel, door_0, bed_0, cell_panel, door_1, bed_1, freedom};
+	
+	private States myState;
+	private TextAsset curText;
+	
+	// Use this for initialization
+	void Start () {
+		myState = States.cell;
+		changeState();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (myState);
+		
+		string methodName = "state_" + myState.ToString();
+		this.SendMessage(methodName);
+	}
+
+	void changeState() {	
+		//update textAsset object and set text object
+		curText = Resources.Load(myState.ToString()) as TextAsset;
+		text.text = curText.ToString();
+	}
+	
+	// STATE FUNCTIONS
+	
+	void state_bed_0 () {		
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+			changeState();
+		}
+	}
+	
+	void state_cell () {			
+		if (Input.GetKeyDown (KeyCode.B)) {
+			myState = States.bed_0;
+			changeState();
+		} else if (Input.GetKeyDown (KeyCode.P)) {
+			myState = States.panel;
+			changeState();
+		}else if (Input.GetKeyDown (KeyCode.D)) {
+			myState = States.door_0;
+			changeState();
+		}
+	}
+	
+	void state_door_0 () {		
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+			changeState();
+		}
+	}
+	
+	void state_panel () {		
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+			changeState();
+		}else if (Input.GetKeyDown (KeyCode.B)) {
+			myState = States.cell_panel;
+			changeState();
+		}
+	}
+	
+	
+}
